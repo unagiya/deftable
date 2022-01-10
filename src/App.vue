25fbd9a36e989281a1547948bfe64d8b07741bcf
unagiya <big.stone.net@gmail.com>
2022-01-11 03:48:12 +0900

    tutorial

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,29 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import { defineComponent } from 'vue';
+//import HelloWorld from './components/HelloWorld.vue';
+import HelloWorld from './components/welcome.vue';
 
-export default {
-  name: 'App',
+/*
+@Options({
+  components: {
+    HelloWorld,
+  },
+})
+export default class App extends Vue {}
+ */
+export default defineComponent({
+  name:'App',
   components: {
     HelloWorld
   }
-}
+});
+
 </script>
 
 <style>
---
+++ src/components/welcome.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hello">
+    <h1>
+      {{ msg }}
+    </h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: "HelloWorld",
+  data(){
+    return {
+      msg: 'Hello TypeScript',
+    }
+  },
+});
+</script>
+
+<style scoped>
+
+</style>
